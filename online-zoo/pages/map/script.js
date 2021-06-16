2cf0da2c47bcf034f72df4bca94177d9c151fac9
DariaShibalko <daryaavramo@yandex.by>
2021-06-16 21:31:18 +0300

    move from private rep

---
+++ online-zoo/pages/map/script.js
@@ -0,0 +1,17 @@
+const panda = document.querySelector(".panda");
+const lemur = document.querySelector(".lemur");
+const gorilla = document.querySelector(".gorilla");
+const eagle = document.querySelector(".eagle");
+ 
+panda.onclick = function(event){
+  window.location.href = "../../../online-zoo/pages/zoos/panda/panda.html";
+}
+lemur.onclick = function(event){
+  window.location.href = "../../../online-zoo/pages/zoos/lemurs/lemur.html";
+}
+gorilla.onclick = function(event){
+  window.location.href = "../../../online-zoo/pages/zoos/gorillas/gorillas.html";
+}
+eagle.onclick = function(event){
+  window.location.href = "../../../online-zoo/pages/zoos/eagles/eagles.html";
+}
